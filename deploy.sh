fa1323cd6b23403175164b5bd47fe4b99d03ee8b
Ernesto Canquiz <fdsoil123@gmail.com>
2021-05-12 23:57:13 +0000

    feat: test1

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,27 @@
 
 # abort on errors
 set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:ejimenez123/ejimenez123.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:ejimenez123/js-vue-my-curriculum.git main:gh-pages
+
+
+
+
+cd -
